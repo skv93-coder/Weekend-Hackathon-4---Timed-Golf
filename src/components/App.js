44fe3032d2c3995f8a0addf14b954ead71b96669
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-11-07 19:35:09 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,12 @@ class Timer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { time: 0, x: 0, y: 0, start: false, date: 0 };
+    this.state = { time: 0, x: 0, y: 0, timer: 0, start: false, date: 0 };
     this.startTimer = this.startTimer.bind(this);
     this.myTimer = this.myTimer.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(ev) {
-    // //console.log(ev);
     if (this.state.x === 250 && this.state.y === 250) {
       return;
     }
@@ -18,36 +17,40 @@ class Timer extends React.Component {
       let z = this.state.x - 5;
       let w = this.state.y;
       this.setState({ x: z });
-      //console.log(ev);
     } else if (ev.keyCode === 38 && this.state.start === true) {
       let z = this.state.x;
       let w = this.state.y - 5;
       this.setState({ y: w });
-      //console.log(ev);
     } else if (ev.keyCode === 39 && this.state.start === true) {
       let z = this.state.x;
       let w = this.state.y;
       this.setState({ x: z + 5 });
-      //console.log(ev);
     } else if (ev.keyCode === 40 && this.state.start === true) {
       let z = this.state.x;
       let w = this.state.y;
       this.setState({ y: w + 5 });
-      //console.log(ev);
     }
   }
   myTimer() {
     if (this.state.x === 250 && this.state.y === 250) {
       return;
     }
+    let v = this.state.date;
+    this.setState({ date: v + 1 });
+    /*if (this.state.date === 0) {
+      this.setState({ timer: Date.now() });
+      this.set
+    } else {
+      let d = Date.now();
+      d = d / 1000 - this.state.timer / 1000;
+      this.setState({ date: d });
+    }*/
 
-    let d = new Date();
-    //console.log(d);
-    let time = d.toLocaleTimeString();
-    this.setState({ date: time });
+    //let time = d.toLocaleTimeString();
   }
   startTimer() {
     this.setState({ start: true });
+
     setInterval(this.myTimer, 1000);
   }
   componentDidMount() {
